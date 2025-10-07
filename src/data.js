a7f537f4d4de0df5a337db87285d9ff04f3d1b1a
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-07 13:22:50 -0700

    Added the data

---
+++ src/data.js
@@ -0,0 +1,100 @@
+const movies = [
+  {
+    name: "La La Land",
+    genre: "Romance",
+    actors: ["Ryan Gosling", "Emma Stone"],
+    length: 128,
+    releaseDate: new Date("2016-12-09"),
+    musical: true
+  },
+  {
+    name: "Inception",
+    genre: "Science Fiction",
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+    length: 148,
+    releaseDate: new Date("2010-07-16"),
+    musical: false
+  },
+  {
+    name: "The Greatest Showman",
+    genre: "Drama",
+    actors: ["Hugh Jackman", "Zac Efron", "Zendaya"],
+    length: 105,
+    releaseDate: new Date("2017-12-20"),
+    musical: true
+  },
+  {
+    name: "The Dark Knight",
+    genre: "Action",
+    actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    length: 152,
+    releaseDate: new Date("2008-07-18"),
+    musical: false
+  },
+  {
+    name: "Mamma Mia!",
+    genre: "Comedy",
+    actors: ["Meryl Streep", "Amanda Seyfried", "Pierce Brosnan"],
+    length: 108,
+    releaseDate: new Date("2008-07-18"),
+    musical: true
+  },
+  {
+    name: "Interstellar",
+    genre: "Adventure",
+    actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+    length: 169,
+    releaseDate: new Date("2014-11-07"),
+    musical: false
+  },
+  {
+    name: "Chicago",
+    genre: "Crime",
+    actors: ["Renée Zellweger", "Catherine Zeta-Jones", "Richard Gere"],
+    length: 113,
+    releaseDate: new Date("2002-12-27"),
+    musical: true
+  },
+  {
+    name: "The Matrix",
+    genre: "Science Fiction",
+    actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+    length: 136,
+    releaseDate: new Date("1999-03-31"),
+    musical: false
+  },
+  {
+    name: "Frozen",
+    genre: "Animation",
+    actors: ["Idina Menzel", "Kristen Bell", "Josh Gad"],
+    length: 102,
+    releaseDate: new Date("2013-11-27"),
+    musical: true
+  },
+  {
+    name: "Gladiator",
+    genre: "Historical Drama",
+    actors: ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+    length: 155,
+    releaseDate: new Date("2000-05-05"),
+    musical: false
+  },
+  {
+    name: "Les Misérables",
+    genre: "Historical",
+    actors: ["Hugh Jackman", "Russell Crowe", "Anne Hathaway"],
+    length: 158,
+    releaseDate: new Date("2012-12-25"),
+    musical: true
+  },
+  {
+    name: "The Shawshank Redemption",
+    genre: "Drama",
+    actors: ["Tim Robbins", "Morgan Freeman"],
+    length: 142,
+    releaseDate: new Date("1994-10-14"),
+    musical: false
+  }
+];
+
+export default movies;
