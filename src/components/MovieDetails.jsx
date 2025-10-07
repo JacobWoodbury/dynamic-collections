f98791b6bbec78ec800c4b218bffec3a905f8f8b
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-07 13:53:02 -0700

    Intial test run

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,5 +1,7 @@
 export default function MovieDetails({length, releaseDate, musical}){
     return(<>
-        
+        <p>{length}</p>
+        <p>{releaseDate}</p>
+        <p>{musical}</p>
     </>)
 }
--- src/components/MovieHead.jsx
+++ src/components/MovieHead.jsx
@@ -1,5 +1,7 @@
 export default function MovieHead({name, genre, actors}){
     return(<>
-    
+        <h3>{name}</h3>
+        <p>{genre}</p>
+        <p>{actors}</p>
     </>)
 }
