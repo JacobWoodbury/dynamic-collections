483bb72859902ad11926ce9fafecd8bd034a8be6
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-07 13:35:57 -0700

    props and imports set up

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,7 +1,19 @@
-export default function Movie({name, genre, actors, length, releaseDate, musical}){
+import MovieHead from "./MovieHead"
+import MovieDetails from "./MovieDetails"
+
+export default function Movie(props){
 
     return(<>
-        
+        <MovieHead
+            name= {props.name}
+            genre= {props.genre}
+            actors = {props.actors}
+        />
+        <MovieDetails
+            length = {props.length}
+            releaseDate = {props.releaseDate}
+            musical = {props.musical}
+        />
     
     </>)
 }
---
+++ src/components/MovieHead.jsx
@@ -0,0 +1,5 @@
+export default function MovieHead({name, genre, actors}){
+    return(<>
+    
+    </>)
+}
