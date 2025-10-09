287c50b2deef38ab890d87c296ccc65cb0c223a4
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-09 12:37:47 -0700

    Added a warning for length

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,12 @@
 export default function MovieDetails({length, releaseDate, musical}){
+    function testLength(length) {
+        if (length > 120) {
+            return "(Over 2 hours)"
+        }
+    }
+    
     return(<>
-        <p>{length}</p>
+        <p>{length} minutes <span className = "warnLength">{testLength(length)}</span></p>
         <p>{releaseDate}</p>
         <p>{musical}</p>
     </>)
