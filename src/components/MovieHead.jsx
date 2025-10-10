b6765ebd329ff38838eced8bfd326bbf2e2d56b7
xavierb117 <194048107+xavierb117@users.noreply.github.com>
2025-10-09 21:01:14 -0700

    Fixed bullet points and added more conditional rendering with genre

--- src/components/MovieHead.jsx
+++ src/components/MovieHead.jsx
@@ -1,13 +1,37 @@
 export default function MovieHead({name, genre, actors}){
+    function featuredGenre() {
+        if (genre === "Science Fiction") {
+            return (
+                <span className = "featured">(Educational!)</span>
+            )
+        }
+        else if (genre === "Crime") {
+            return (
+                <span className = "featured">(Eerie!)</span>
+            )
+        }
+        else if (genre == "Comedy") {
+            return (
+                <span className = "featured">(Funny!)</span>
+            )
+        }
+        else {
+            return (
+                <span className = "featured">(Amazing!)</span>
+            )
+        }
+    }
+
     const example = actors.map((actor) => {
         return (
             <li>{actor}</li>
         )
-    }) 
+    })
+
     return(<>
         <h3>{name}</h3>
-        <p>{genre}</p>
-        <ul>
+        <p>{genre} {featuredGenre()}</p>
+        <ul class = "centeredBullets">
             {example}
         </ul>
     </>)
